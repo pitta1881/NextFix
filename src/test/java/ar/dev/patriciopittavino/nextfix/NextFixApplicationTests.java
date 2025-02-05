acf463dddd033f5be853a82b132a63da4213d70f
Patricio Pittavino <pitta1881@gmail.com>
2025-02-05 20:43:44 -0300

    first commit

---
+++ src/test/java/ar/dev/patriciopittavino/nextfix/NextFixApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.patriciopittavino.nextfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
