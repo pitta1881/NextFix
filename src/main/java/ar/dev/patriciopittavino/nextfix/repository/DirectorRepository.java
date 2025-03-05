508028c4388bbd56389e95aa9c7bba76b4ab0dc0
pitta1881 <pitta1881@users.noreply.github.com>
2025-03-05 22:43:07 +0000

    deploy: 917f23cca6a3f5855fb273440e99914370e18179

--- src/main/java/ar/dev/patriciopittavino/nextfix/repository/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.patriciopittavino.nextfix.repository;
-
-import ar.dev.patriciopittavino.nextfix.model.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectorRepository extends JpaRepository<Director, Long> {
-}
