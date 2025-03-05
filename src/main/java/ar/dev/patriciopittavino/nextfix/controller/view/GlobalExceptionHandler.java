f21b14c812c54eeaf4d8067131aabc7180e43ec7
Patricio Pittavino <pitta1881@gmail.com>
2025-03-05 20:45:16 -0300

    add global exception page and controller

---
+++ src/main/java/ar/dev/patriciopittavino/nextfix/controller/view/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package ar.dev.patriciopittavino.nextfix.controller.view;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        return "errors/global";
+    }
+}
